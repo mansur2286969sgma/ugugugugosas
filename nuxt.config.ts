bcc34d239acfe29d562652e03caedf0a7558f790
emngr256 <alan27945@gmail.com>
2025-09-25 03:28:55 +0500

    1.5

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,4 @@
-﻿// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
+﻿export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss'],
@@ -8,12 +7,16 @@ export default defineNuxtConfig({
     head: {
       script: [
         {
-          innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','G-MPRBV4HW0D');`
-        }
-      ],
-      noscript: [
+          src: 'https://www.googletagmanager.com/gtag/js?id=G-MPRBV4HW0D',
+          async: true
+        },
         {
-          innerHTML: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WHRMV4CZ" height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+          innerHTML: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-MPRBV4HW0D');
+          `
         }
       ]
     }
